5de0547555527e61f03593a9bfb8cf98ce46cf0d
iararoldan99 <iararroldan@gmail.com>
2024-10-28 01:37:56 -0300

    configuraciones

--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,12 @@
+const { connectDB } = require('./db.js');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+// Conectar a la base de datos
+connectDB();
+
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
